3d3c7c20ecc5f3400933c847945d0edd74cd8787
chalanasangeeth36 <chalanasthisera@gmail.com>
2024-01-17 10:10:18 +0530

    form

---
+++ src/Componant/Lable.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Lable = (props) => {
+  return (
+    <div className="lable text-[#0d2a64] font-['Poppins'] font-semibold leading-[140%]">
+      {props.lable}
+    </div>
+  );
+};
+
+export default Lable;
